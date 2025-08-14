11e5c9052b94baa1783bdfc7f4d5e3b833775d9a
davewise-codes <perezmartinez.david@gmail.com>
2025-08-14 23:36:01 +0200

    feat(web): integrate i18n

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,48 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Button, Alert, AlertTitle, AlertDescription } from "@hikai/ui";
+import { useTranslation } from "@hikai/i18n";
 
-import "./App.css";
-
-function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<>
-			<div>
-				<Alert>
-					<AlertTitle>Heads up!</AlertTitle>
-					<AlertDescription>
-						You can add components to your app using the cli.
-					</AlertDescription>
-				</Alert>
-			</div>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<div className="p-8">
-				<Button>Test Button</Button>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+export default function App() {
+  const { t } = useTranslation();
+  return <div>{t("placeholder")}</div>;
 }
-
-export default App;
