b4af6eb555d69c7d39942304a869e5d90499bc3f
davewise-codes <perezmartinez.david@gmail.com>
2025-08-17 07:52:39 +0200

    feat(i18n): add core module

--- packages/i18n/src/client.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-export {
-	useTranslation,
-	I18nextProvider,
-	initReactI18next,
-} from "react-i18next";
-// Si más adelante necesitas un Provider central, puedes exportarlo aquí.
